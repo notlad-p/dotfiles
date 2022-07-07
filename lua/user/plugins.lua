205b2e5f7d3a20ecd7ad168971ae0c346ec0e5be
Dalton Perkins <contact@daltonp.dev>
2022-07-06 20:54:07 -0400

    Added packer and basic plugins

---
+++ lua/user/plugins.lua
@@ -0,0 +1,85 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+return require("packer").startup(function()
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+
+	-- File tree with icons
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+		config = function()
+			require("nvim-tree").setup()
+		end,
+	})
+
+	-- onedark theme
+	use({
+		"navarasu/onedark.nvim",
+		as = "onedark",
+		config = function()
+			local onedark = require("onedark")
+
+			onedark.setup({
+				style = "deep",
+			})
+
+			onedark.load()
+		end,
+	})
+
+	-- treesitter styntax highlighting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("user.treesitter").config()
+		end,
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+M.config = function()
+	local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+
+	if not status_ok then
+		print("Treesitter not ready")
+		return
+	end
+
+	treesitter_configs.setup({
+		ensure_installed = {
+			"javascript",
+			"typescript",
+			"tsx",
+			"json",
+			"css",
+			"java",
+			"lua",
+			"python",
+		},
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+		indent = { enable = true },
+	})
+end
+
+return M
