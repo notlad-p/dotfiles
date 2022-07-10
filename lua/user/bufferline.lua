e00850fbb4048926df5c69ae838089a601e1c344
Dalton Perkins <contact@daltonp.dev>
2022-07-10 19:43:27 -0400

    Fixed undefined variable bug

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -3,9 +3,6 @@ local M = {}
 local function diagnostics_indicator(num, _, diagnostics, _)
 	local result = {}
 	local symbols = { error = "", warning = "", info = "" }
-	if not lvim.use_icons then
-		return "(" .. num .. ")"
-	end
 	for name, count in pairs(diagnostics) do
 		if symbols[name] and count > 0 then
 			table.insert(result, symbols[name] .. " " .. count)
