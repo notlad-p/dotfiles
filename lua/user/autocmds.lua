1ed8872e13fc36bb964118845871fa49c0435661
Dalton Perkins <contact@daltonp.dev>
2022-07-20 20:42:08 -0400

    Added auto commands for highlighting on yank and markdown spell checking

---
+++ lua/user/autocmds.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.setup = function()
+	local autocmds = {
+		{
+			"TextYankPost",
+			{
+				group = "_general_settings",
+				pattern = "*",
+				desc = "Highlight text on yank",
+				callback = function()
+					require("vim.highlight").on_yank({ higroup = "Search", timeout = 200 })
+				end,
+			},
+		},
+		{
+			"FileType",
+			{
+				group = "_filetype_settings",
+				pattern = { "gitcommit", "markdown" },
+				desc = "spell check gitcommit and markdow files",
+				command = "setlocal  spell",
+			},
+		},
+	}
+
+	M.define_autocmds(autocmds)
+end
+
+--- Create autocommand groups based on the passed definitions
+--- Also creates the augroup automatically if it doesn't exist
+---@param definitions table contains a tuple of event, opts, see `:h nvim_create_autocmd`
+function M.define_autocmds(definitions)
+	for _, entry in ipairs(definitions) do
+		local event = entry[1]
+		local opts = entry[2]
+		if type(opts.group) == "string" and opts.group ~= "" then
+			local exists, _ = pcall(vim.api.nvim_get_autocmds, { group = opts.group })
+			if not exists then
+				vim.api.nvim_create_augroup(opts.group, {})
+			end
+		end
+		vim.api.nvim_create_autocmd(event, opts)
+	end
+end
+
+return M
