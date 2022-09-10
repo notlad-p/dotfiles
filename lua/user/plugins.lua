a49209d6bef601e6d5302809ebfaf0b651bbbbbc
Dalton Perkins <contact@daltonp.dev>
2022-09-09 20:58:04 -0400

    added impatient.nvim and sniprun

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -313,6 +313,9 @@ return require("packer").startup(function(use)
 		end,
 	})
 
+	use({ "michaelb/sniprun", run = "bash ./install.sh" })
+
+	use("lewis6991/impatient.nvim")
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
