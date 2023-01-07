7947d863acdf9bffabd3f920808595ff56ab5064
Dalton Perkins <contact@daltonp.dev>
2023-01-07 16:47:57 -0500

    added personal.sh script to clone personal repos

--- clone.sh
+++ clone.sh
@@ -5,8 +5,3 @@
 git clone https://github.com/notlad-p/nvim.git ~/.config/nvim
 # awesomewm
 git clone --recurse-submodules https://github.com/notlad-p/awesome.git ~/.config/awesome
-# obsidian
-# git clone https://github.com/notlad-p/obsidian-vault.git ~/Documents/obsidian-vault
-
-# OTHERS - DElETE LATER
-git clone https://github.com/notlad-p/lad-schemes.nvim.git ~/projects/lad-schemes.nvim
---
+++ personal.sh
@@ -0,0 +1,9 @@
+# ------------------------------------------------------
+# Clone personal repos
+# ------------------------------------------------------
+
+# obsidian
+git clone https://github.com/notlad-p/obsidian-vault.git ~/Documents/obsidian-vault
+
+# OTHERS - DElETE LATER
+git clone https://github.com/notlad-p/lad-schemes.nvim.git ~/projects/lad-schemes.nvim
