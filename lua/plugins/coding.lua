c0ba1d9dc21076a8b9e69aef5edfce3c203bf332
Dalton Perkins <contact@daltonp.dev>
2023-01-27 06:53:34 -0500

    moved autopairs setup next to its declaration

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,7 +1,6 @@
 -- windwp/nvim-autopairs : mini.pairs
--- tpope/vim-surround : mini.surround
--- michaeljsmith/vim-indent-object : mini.ai
--- numToStr/Comment.nvim : mini.comment
+-- tpope/vim-surround : nvim-surround
+-- michaeljsmith/vim-indent-object : mini.ai : nvim-treesitter-textobjects
 return {
   -- comments
   {
@@ -22,23 +21,22 @@ return {
   },
 
   -- autopairs / tags
+  -- TODO: insert '()' after functions and methods using cmp + nvim-autopairs?
+  -- https://github.com/windwp/nvim-autopairs#you-need-to-add-mapping-cr-on-nvim-cmp-setupcheck-readmemd-on-nvim-cmp-repo
   {
     "windwp/nvim-autopairs",
-    config = function()
-      require("user.autopairs").setup()
-    end,
-    event = "InsertEnter",
+    event = "VeryLazy",
+    opts = {
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false,
+      },
+      fast_wrap = {},
+    },
   },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup {
-        autotag = {
-          enable = true,
-        },
-      }
-    end,
-  },
+  "windwp/nvim-ts-autotag",
 
   -- adds surroundings text object
   "tpope/vim-surround",
@@ -50,6 +48,11 @@ return {
   {
     "jose-elias-alvarez/typescript.nvim",
     config = function()
+      -- TODO: create autocmd for TS, TSX, JS, JSX filetypes
+      -- that adds typescript util keybinds under T
+      -- -----OR------
+      -- Use null-ls to add commands to code action menu using null-ls!!!
+      -- https://github.com/jose-elias-alvarez/typescript.nvim#integrations
       require("typescript").setup {
         disable_commands = false, -- prevent the plugin from creating Vim commands
         debug = false, -- enable debug logging for commands
