e77f998aa1f878f212bee64d3172332da8e0b367
Dalton Perkins <contact@daltonp.dev>
2025-04-12 11:01:07 -0400

    update yazi funciton

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -16,11 +16,11 @@ function zsh_add_plugin() {
   source $HOME/Packages/$PLUGIN_NAME/$PLUGIN_NAME.zsh
 }
 
-function ya() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
 	fi
 	rm -f -- "$tmp"
 }
