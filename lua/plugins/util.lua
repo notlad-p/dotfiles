3b4b77f19bb40b37a4844b3ec13c2ec9c25f71cc
Dalton Perkins <contact@daltonp.dev>
2025-04-21 09:37:31 -0400

    remap persistence keymaps and change snacks picker layout

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,44 +3,56 @@ return {
     "folke/persistence.nvim",
     event = "BufReadPre",
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "<leader>qs", false},
-    { "<leader>qS", false},
-    { "<leader>ql", false},
-    { "<leader>qd", false},
+    keys = {
+      { "<leader>qs", false },
+      { "<leader>qS", false },
+      { "<leader>ql", false },
+      { "<leader>qd", false },
 
-    { "<leader>Qs", function() require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>QS", function() require("persistence").select() end,desc = "Select Session" },
-    { "<leader>Ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>Qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-  },
+      {
+        "<leader>Qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore Session",
+      },
+      {
+        "<leader>QS",
+        function()
+          require("persistence").select()
+        end,
+        desc = "Select Session",
+      },
+      {
+        "<leader>Ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore Last Session",
+      },
+      {
+        "<leader>Qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Don't Save Current Session",
+      },
+    },
   },
   {
+    -- NOTE: useful keybinds:
+    -- Alt w: cycle window (to results)
+    -- Alt s: use flash to pick result
     "snacks.nvim",
-    -- preset = "ivy_split",
+    ---@type snacks.Config
     opts = {
-
+      -- @type snacks.picker.Config
       picker = {
-
-        -- layout = {
-        --   backdrop = false,
-        --   row = 1,
-        --   width = 0.4,
-        --   min_width = 80,
-        --   height = 0.8,
-        --   border = "none",
-        --   box = "vertical",
-        --   { win = "preview", title = "{preview}", height = 0.4, border = "rounded" },
-        --   {
-        --     box = "vertical",
-        --     border = "rounded",
-        --     title = "{title} {live} {flags}",
-        --     title_pos = "center",
-        --     { win = "input", height = 1, border = "bottom" },
-        --     { win = "list", border = "none" },
-        --   },
-        -- },
+        layout = {
+          preset = "ivy_split",
+          -- preset = "left",
+          -- preset = "right",
+        },
       },
     },
   },
