c0f823413be9f64d064e5602d9c0b7f196d7d976
Dalton Perkins <contact@daltonp.dev>
2025-05-11 13:14:09 -0400

    connect bar audio icons to WirePlumber

--- widget/Bar/AudioInOut.tsx
+++ widget/Bar/AudioInOut.tsx
@@ -1,11 +1,35 @@
+import { bind, Variable } from "astal";
 import { App, Gdk, Gtk } from "astal/gtk4";
 import { AudioSettingsWindowName } from "../AudioSettings/AudioSettings";
+import Wp from "gi://AstalWp";
+
+import getVolumeIcon from "../../utils/volume-icon";
 
 type AudioInOutProps = {
   monitor: Gdk.Monitor;
 };
 const AudioInOut = ({ monitor }: AudioInOutProps) => {
   const monitorName = monitor.get_connector();
+
+  const wp = Wp.get_default()?.audio;
+
+  const defaultSpeakerMuted = bind(wp.defaultSpeaker, "mute");
+  const defaultSpeakerVolume = bind(wp.defaultSpeaker, "volume");
+  const speakerIcon = Variable.derive(
+    [defaultSpeakerMuted, defaultSpeakerVolume],
+    (isMuted, volumeVal) => {
+      return getVolumeIcon(false, isMuted, volumeVal);
+    },
+  );
+
+  const defaultMicMuted = bind(wp.defaultMicrophone, "mute");
+  const defaultMicVolume = bind(wp.defaultMicrophone, "volume");
+  const micIcon = Variable.derive(
+    [defaultMicMuted, defaultMicVolume],
+    (isMuted, volumeVal) => {
+      return getVolumeIcon(true, isMuted, volumeVal);
+    },
+  );
 
   return (
     <button
@@ -20,8 +44,8 @@ const AudioInOut = ({ monitor }: AudioInOutProps) => {
       }}
     >
       <box spacing={10}>
-        <image iconName="volume-high-symbolic" />
-        <image iconName="mic-off-symbolic" />
+        <image iconName={bind(speakerIcon)} />
+        <image iconName={bind(micIcon)} />
       </box>
     </button>
   );
