287505e277e73c403f7e80d3b0efe6c4836f4c2d
Dalton Perkins <contact@daltonp.dev>
2025-05-12 16:04:12 -0400

    update widget initializer

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
-import { App, Widget } from "astal/gtk4";
+import { App } from "astal/gtk4";
 import style from "./styles/index.scss";
+
 import Bar from "./widget/Bar/Bar";
 import LocalInfo from "./widget/LocalInfo/LocalInfo";
 import AudioSettings from "./widget/AudioSettings/AudioSettings";
+import OperatingSystem from "./widget/OperatingSystem/OperatingSystem";
 
 App.start({
   css: style,
   icons: "./icons",
   main() {
-    App.get_monitors().map(Bar);
-    App.get_monitors().map(LocalInfo);
-    App.get_monitors().map(AudioSettings);
+    const windows = [Bar, LocalInfo, AudioSettings, OperatingSystem];
+
+    windows.map((win) => App.get_monitors().map(win));
   },
 });
