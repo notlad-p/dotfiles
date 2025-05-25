a2109db58e22935759b7260f6fe79e7028d9bc7b
Dalton Perkins <contact@daltonp.dev>
2025-05-25 14:23:20 -0400

    update tailwind config to v4

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+// this is only here to trigger tailwindcss-language-server
+// see my neovim LSP config to mimic this behavior
+//
+// config reference: https://v3.tailwindcss.com/docs/configuration
+
+// TODO: put custom colors / classes here
+module.exports = {};
