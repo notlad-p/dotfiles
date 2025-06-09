b93a7a819b4c4b940cc3b02055b38448acf59e53
Dalton Perkins <contact@daltonp.dev>
2025-06-09 13:19:54 -0400

    feat(ags): create monitor change util funciton
    Handle multiple monitor changes through debouncing

--- config/ags/app.ts
+++ config/ags/app.ts
@@ -9,55 +9,41 @@ import PowerMenu from "./widget/OperatingSystem/PowerMenu";
 import QuickSettings from "./widget/QuickSettings/QuickSettings";
 
 import style from "./styles/index.scss";
-import { timeout } from "astal";
+import handleMonitorChange from "./utils/monitor-change";
+
+// creates widget windows on each screen
+export const createWindows = () => {
+  const windows = [
+    Bar,
+    LocalInfo,
+    AudioSettings,
+    OperatingSystem,
+    PowerMenu,
+    QuickSettings,
+  ];
+
+  for (const gdkmonitor of App.get_monitors()) {
+    windows.map((win) => win(gdkmonitor));
+  }
+};
 
 App.start({
   css: style,
   icons: "./icons",
   main() {
-    const windows = [
-      Bar,
-      LocalInfo,
-      AudioSettings,
-      OperatingSystem,
-      PowerMenu,
-      QuickSettings,
-    ];
-
     const hyprland = Hyprland.get_default();
 
-    for (const gdkmonitor of App.get_monitors()) {
-      windows.map((win) => win(gdkmonitor));
-    }
+    createWindows();
 
     // handle monitor add / remove by refreshing windows
     hyprland.connect("monitor-added", (_, monitor: Hyprland.Monitor) => {
-      console.log("ADD");
-      console.log(monitor.get_name());
-
-      App.get_windows().forEach((window) => window?.destroy());
-
-      timeout(2000, () => {
-        for (const gdkmonitor of App.get_monitors()) {
-          windows.map((win) => win(gdkmonitor));
-        }
-      });
+      console.log("Add monitor: ", monitor.get_name());
+      handleMonitorChange();
     });
 
-    hyprland.connect(
-      "monitor-removed",
-      (hypr: Hyprland.Hyprland, monitorId: number) => {
-        console.log("REMOVE");
-        console.log(monitorId);
-
-        App.get_windows().forEach((window) => window?.destroy());
-
-        timeout(2000, () => {
-          for (const gdkmonitor of App.get_monitors()) {
-            windows.map((win) => win(gdkmonitor));
-          }
-        });
-      },
-    );
+    hyprland.connect("monitor-removed", (_, monitorId: number) => {
+      console.log("Remove monitor id: ", monitorId);
+      handleMonitorChange();
+    });
   },
 });
